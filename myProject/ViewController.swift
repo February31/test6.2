789c6d89cc55547f27037ca8f8b2bff20be9de90
Wu <wu@wudemacbook-air.local>
2018-12-18 13:53:54 +0800

    Initial Commit

---
+++ myProject/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by student on 2018/10/24.
+//  Copyright © 2018年 sicnu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        let redview=UIView()
+//        redview.backgroundColor=UIColor.red
+//        let greenview=UIView()
+//        greenview.backgroundColor=UIColor.green
+//        let yellowview=UIView()
+//        yellowview.backgroundColor=UIColor.yellow
+//        
+//        let stackview=UIStackView(arrangedSubviews: [redview,greenview,yellowview])
+//        
+//        stackview.axis = .horizontal
+//        stackview.alignment = .fill
+//        stackview.distribution = .fillEqually
+//        stackview.spacing=20
+//        
+//        stackview.frame=CGRect(x: 20, y: 20, width: 20, height: 20)
+//        
+//        view.addSubview(stackview)
+//        
+//        stackview.translatesAutoresizingMaskIntoConstraints = false
+//        stackview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive=true
+//        stackview.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive=true
+//        stackview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20).isActive=true
+//        stackview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive=true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
